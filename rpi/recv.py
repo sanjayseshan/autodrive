f2bf04551559149fdf323f13bc794d602df04e2b
Sanjay Seshan <sanjay.seshan@outlook.com>
2018-12-30 01:27:52 +0000

    pi tracker

--- rpi/recv.py
+++ rpi/recv.py
@@ -8,12 +8,17 @@ import time
 import atexit
 
 #connect to server
-HOST = '192.168.1.15'    # The remote host
+HOST = '192.168.1.30'    # The remote host
 PORT = 5000             # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("",PORT))
 
 address = (HOST, PORT)
+
+cam0=[0,0]
+cam1=[0,0]
+picam=[0,0]
+
 #s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 try: 
    s.sendto("HELLO".encode(),address)
@@ -39,14 +44,7 @@ RMotor.run(Adafruit_MotorHAT.FORWARD)
 LMotor.run(Adafruit_MotorHAT.BACKWARD)
                 
 
-def process_msg(data):
-    # Move motors at power sent from server
-    print(data.decode())
-    power = data.decode().split(';')
-    power[0] = int(power[0])/3
-    power[1] = int(power[1])/3
-    LMotor.setSpeed(power[0])
-    RMotor.setSpeed(power[1])
+
     
 while True:
          # recieve messages from server
@@ -55,7 +53,22 @@ while True:
             indata = s.recvfrom(1500)
             print(indata)
             data,tmp = indata
-            process_msg(data)
+            ip,port=tmp
+            power = data.decode().split(';')
+            print(ip+":"+str(port)+" --> "+data.decode())
+            power[0] = int(power[0])/3
+            power[1] = int(power[1])/3
+            if ip == "127.0.0.1":
+               picam = power
+            elif ip == "192.168.1.30" and port == 4000:
+               cam0 = power
+            elif ip == "192.168.1.30" and port == 4001:
+               cam1 = power
+               # Move motors at power sent from server
+            avgpower = [(picam[0]+cam0[0]+cam1[0])/3,(picam[1]+cam0[1]+cam1[1])/3]
+            print("L: "+str(avgpower[0])+" R: "+str(avgpower[1]))
+#            LMotor.setSpeed(avgpower[0])
+#            RMotor.setSpeed(avgpower[1])
          except Exception as e:
             print("FAILURE TO RECV.." + str(e.args) + "..RECONNECTING")
             try:
