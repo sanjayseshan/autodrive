e3ff082e581bed23b110945038e49cd2820c50bb
Sanjay Seshan <sanjay.seshan@outlook.com>
2019-02-24 20:37:47 -0500

    edits to make d and p correction better, logs, and screenshots

--- tracking.new.py
+++ tracking.new.py
@@ -7,6 +7,7 @@ import struct
 from time import sleep
 import sys
 import time
+import math
 
 # set up network socket/addresses
 host = '192.168.1.24'
@@ -39,7 +40,7 @@ if sys.argv[1] == "0" or sys.argv[1] == "1": # for new Logitech camera
     upper_orange = np.array([40,255,255])
 
     # set up camera
-    cam = cv2.VideoCapture(int(sys.argv[1]))
+    #cam = cv2.VideoCapture(int(sys.argv[1]))
 else: # for old Logitech camera
     lower_green=np.array([75,60,50])
     upper_green=np.array([105,220,140])
@@ -83,22 +84,27 @@ def on_mouse_click (event, x, y, flags, frame):
         print(thiscol)
         print(frame[y,x].tolist())
         if thiscol == "green":
-            lower_green=np.array([frame[y,x].tolist()[0]-20,frame[y,x].tolist()[1]-20,frame[y,x].tolist()[2]-20])
-            upper_green=np.array([frame[y,x].tolist()[0]+20,frame[y,x].tolist()[1]+20,frame[y,x].tolist()[2]+20])
+            lower_green=np.array([frame[y,x].tolist()[0]-20,frame[y,x].tolist()[1]-30,frame[y,x].tolist()[2]-50])
+            upper_green=np.array([frame[y,x].tolist()[0]+20,frame[y,x].tolist()[1]+30,frame[y,x].tolist()[2]+50])
             thiscol = "red"
         elif thiscol == "red":
-            lower_red=np.array([frame[y,x].tolist()[0]-20,frame[y,x].tolist()[1]-20,frame[y,x].tolist()[2]-20])
-            upper_red=np.array([frame[y,x].tolist()[0]+20,frame[y,x].tolist()[1]+20,frame[y,x].tolist()[2]+20])
-            thiscol = "black"
-        elif thiscol == "black":
-            lower_black=np.array([frame[y,x].tolist()[0]-255,frame[y,x].tolist()[1]-100,frame[y,x].tolist()[2]-100])
-            upper_black=np.array([frame[y,x].tolist()[0]+70,frame[y,x].tolist()[1]+20,frame[y,x].tolist()[2]+20])
+            lower_red=np.array([frame[y,x].tolist()[0]-20,frame[y,x].tolist()[1]-30,frame[y,x].tolist()[2]-50])
+            upper_red=np.array([frame[y,x].tolist()[0]+20,frame[y,x].tolist()[1]+30,frame[y,x].tolist()[2]+50])
             thiscol = "none"
+        elif thiscol == "black":
+            #lower_black=np.array([0,0,0])
+            #upper_black=np.array([180,frame[y,x].tolist()[1]+50,frame[y,x].tolist()[2]+40])
+            lower_black=np.array([0,0,0])
+            upper_black=np.array([180,70,80])
+            thiscol = "none"
+
 thiscol = "green"
 
-def main():
+def calibrate():
     capture = cv2.VideoCapture(camid)
-    global thiscol
+    global thiscol,lower_black,upper_black
+    lower_black=np.array([0,0,0])
+    upper_black=np.array([180,70,80])
     while thiscol != "none":
         ret, cap_img=cam.read()
         img=cv2.resize(cap_img,(xdim,ydim))
@@ -112,7 +118,7 @@ def main():
             cv2.putText(img, str("CLICK ON BLACK"), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
             
         if colors:
-            cv2.putText(img, str(colors[-1]), (10, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+            cv2.putText(img, "LAST: "+str(colors[-1]), (10, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
         cv2.imshow('frame', img)
         cv2.setMouseCallback('frame', on_mouse_click, hsv)
         
@@ -122,11 +128,18 @@ def main():
     capture.release()
     cv2.destroyAllWindows()
 
-main()
+calibrate()
     
 while True:
     cv2.waitKey(10)
-    
+
+    try:
+        cv2.putText(img, "green: "+str(colors[0]), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        cv2.putText(img, "orange: "+str(colors[1]), (10, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        #cv2.putText(img, "black: "+str(colors[2]), (10, 150), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        cv2.imshow("robotimg"+camid,img)
+    except:
+        pass
     # grab image, resize, save a copy and convert to HSV
     ret, cap_img=cam.read()
     img=cv2.resize(cap_img,(xdim,ydim))
@@ -134,6 +147,24 @@ while True:
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     #cv2.imshow("cam_raw"+camid,img)
 
+    
+    """   
+    hsv = imgHSV.copy()
+    if thiscol == "green":
+        cv2.putText(img, str("CLICK ON GREEN"), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+    if thiscol == "red":
+        cv2.putText(img, str("CLICK ON ORANGE"), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+    if thiscol == "black":
+        cv2.putText(img, str("CLICK ON BLACK"), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+            
+    if colors:
+        cv2.putText(img, "LAST: "+str(colors[-1]), (10, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+#    cv2.imshow('calibrate'+camid, img)
+    cv2.setMouseCallback('robotimg'+camid, on_mouse_click, hsv)
+    """
+
+
+    
     # identify the green regions --> estimates the position of the robot
     greenmask=cv2.inRange(imgHSV,lower_green,upper_green)
     # this removes noise by eroding and filling in the regions
@@ -189,13 +220,15 @@ while True:
         #print  best_redcont+[max(greencx-300,0),max(greency-200,0)]
         cv2.drawContours(img, best_redcont+[max(greencx-300,0),max(greency-200,0)], -1, (0,255,0), 3)
         M = cv2.moments(best_redcont)
-        redcx,redcy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+        redcx_incrop,redcy_incrop = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+        redcx = redcx_incrop+max(greencx-300,0);
+        redcy = redcy_incrop+max(greency-200,0);
     #cv2.imshow("red",img)
 
     if not (redconts and greenconts and gmax > 5000 and rmax > 5000):
         # if robot not found --> done
         data = str(0) + ";" + str(0) + ";" + str(0)
-        print("P, I, D, (E), (T) --->", 0, 0, 0, 0, time.time())
+        #print("P, I, D, (E), (T) --->", 0, 0, 0, 0, time.time())
 
 
         # send movement fix to robot
@@ -216,32 +249,35 @@ while True:
     # I need to special case of 90/-90 due to tan() discontinuity
     # I also need to deal with angles > 90 and < 0 to map correctly
     # to a 360 degree circle
-    if (greencx-(redcx+max(greencx-300,0))) == 0:
-        if greency > redcy+max(greency-200,0):
+    if (greencx-redcx) == 0:
+        if greency > redcy:
             ang = 90
         else:
             ang = 270
     else:
-        Tredcy = ydim - (redcy+max(greency-200,0))
+        Tredcy = ydim - redcy
         Tgreency = ydim - greency
-        ang = 180/np.pi * np.arctan((Tredcy-Tgreency)/((redcx+max(greencx-300,0))-greencx))
-        if greencx > redcx+max(greencx-300,0):
+#        ang = 180/np.pi * np.arcsin((redcy-greency)/(math.sqrt((redcx-greencx)*(redcx-greencx)+(redcy-greency)*(redcy-greency))))
+        ang = 180/np.pi * np.arctan(float(redcy-greency)/float(redcx-greencx))        
+        if greencx > redcx:
             ang = 180 + ang
         elif ang < 0:
             ang = 360 + ang
-
+        #print(ang, redcy, greency, redcx, greencx)
+        ang = 360-ang
+    #print(ang)
+    
     # draw some robot lines on the screen and display
-    cv2.line(img, (greencx,greency), (redcx+max(greencx-300,0),redcy+max(greency-200,0)), (200,0,200),3)
+    cv2.line(img, (greencx,greency), (redcx,redcy), (200,0,200),3)
     #cv2.imshow("robotimg",robotimg)
 
     #cv2.imshow("cam"+camid,img)
 
     # find a small region in front of the robot and crop that part of the image
-    ylen = (greency-(redcy+max(greency-300,0)))
-    xlen = (greencx-(redcx+max(greencx-300,0)))
-    boxX = (redcx+max(greencx-300,0)) - xlen/1.6
-    boxY = (redcy+max(greency-200,0)) - ylen/1.6
-    
+    ylen = (greency-redcy)
+    xlen = (greencx-redcx)
+    boxX = redcx - xlen/2
+    boxY = redcy - ylen/2
     if boxX > (xdim-cropsize):
         Xcropsize = xdim - boxX
     elif boxX < cropsize:
@@ -255,11 +291,15 @@ while True:
         Ycropsize = boxY
     else:
         Ycropsize = cropsize
+    #print(xlen, ylen, boxX, boxY, Xcropsize, Ycropsize)
     
     crop_img = img[int(abs(boxY-Ycropsize)):int(abs(boxY+Ycropsize)), int(abs(boxX-Xcropsize)):int(abs(boxX+Xcropsize))]
     #cv2.imshow("CROP",crop_img)
     # find the black regions in the cropped image (this is the line)
-    blackmask=cv2.inRange(crop_img,lower_black,upper_black)
+    try:
+        blackmask=cv2.inRange(crop_img,lower_black,upper_black)
+    except:
+        continue
     blackmaskOpen=cv2.morphologyEx(blackmask,cv2.MORPH_OPEN,kernelOpen)
     blackmaskClose=cv2.morphologyEx(blackmaskOpen,cv2.MORPH_CLOSE,kernelClose)
     blackconts, h = cv2.findContours(blackmaskClose, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -278,7 +318,7 @@ while True:
     if not (blackconts and max_area > 200):
         # skip if didn't find a line
         data = str(0) + ";" + str(0) + ";" + str(0)
-        print("P, I, D, (E), (T) --->", 0, 0, 0, 0, time.time())
+        #print("P, I, D, (E), (T) --->", 0, 0, 0, 0, time.time())
 
 
         # send movement fix to robot
@@ -303,20 +343,25 @@ while True:
     (x_min, y_min), (w_min, h_min), lineang = blackbox
     # Unfortunately, opencv only gives rectangles angles from 0 to -90 so we
     # need to do some guesswork to get the right quadrant for the angle
+    #print(lineang, ang, x_min, y_min, w_min, h_min)
     if w_min > h_min:
         if (ang > 135):
             lineang = 180 - lineang
         else:
             lineang = -1 * lineang
     else:
-        if (ang > 225):
+        if (ang > 270) or (ang < 45):
             lineang = 270 - lineang
         else:
             lineang = 90 - lineang
+    #print(lineang,ang)
 
     # draw a line with the estimate of line location and angle
-    cv2.line(crop_img, (int(x_min),int(y_min)), (int(x_min+50*np.cos(lineang*np.pi/180)),int(y_min-50*np.sin(lineang*np.pi/180))), (200,0,200),2)
+    x_end = int(x_min-50*np.cos(lineang*np.pi/180))
+    y_end = int(y_min+50*np.sin(lineang*np.pi/180))
+    cv2.line(crop_img, (int(x_min),int(y_min)), (x_end,y_end), (200,0,200),2)
     cv2.circle(crop_img,(int(x_min),int(y_min)),3,(200,0,200),-1)
+    cv2.line(crop_img, (int(x_min),int(y_min)), (Xcropsize,Ycropsize), (200,0,200),2)
 
     #try:
     #    cv2.imshow("boxlineangle"+camid, crop_img)
@@ -333,7 +378,7 @@ while True:
     elif D_fix > 90:
         D_fix -= 180
 
-    # the position error is an estimate of how far the font center of the
+    # the position error is an estimate of how far the front center of the
     # robot is from the line. The center of the cropped image
     # (x,y) = (cropsize, cropsize) is the front of the robot. (x_min, y_min) is
     # the center of the line. Draw a line from the front center of the robot
@@ -345,7 +390,7 @@ while True:
         else:
             P_fix = 270 - ang
     else:
-        temp_angle = 180/np.pi * np.arctan((cropsize - y_min)/(x_min - cropsize))
+        temp_angle = 180/np.pi * np.arctan(float(Ycropsize - y_min)/float(x_min - Xcropsize))
         if (temp_angle < 0):
             if (ang > 225):
                 temp_angle = 360 + temp_angle
@@ -354,6 +399,7 @@ while True:
         elif (ang > 135 and ang < 315):
                 temp_angle = 180 + temp_angle
         P_fix = temp_angle - ang
+        #print(temp_angle, ang)
     if (P_fix > 180):
         P_fix = P_fix - 360
     elif P_fix < -180:
@@ -370,19 +416,15 @@ while True:
     lastP_fix = P_fix
     
     # print and save correction and current network conditions
-    error = 100*max_area/1400
+    error = 100*max_area/1300
     print("P, I, D, (E), (T) --->", P_fix, I_fix, D_fix, error, time.time())
 #    tmpos = os.popen('echo seshan | sudo -S tc qdisc show dev wlp7s0').read()
 #    print(tmpos)
 
     # Compute correction based on angle/position error
-    left = int(100 - 2.5*P_fix - 1*D_fix - 0.02*I_fix)
-    right = int(100 + 2.5*P_fix + 1*D_fix + 0.02*I_fix)
+    left = int(100 - 1.0*P_fix - .5*D_fix - 0.02*I_fix)
+    right = int(100 + 1.0*P_fix + .5*D_fix + 0.02*I_fix)
     data = str(left) + ";" + str(right) + ";" + str(error)
-    try:
-        cv2.imshow("robotimg"+camid,img)
-    except:
-        pass
      # send movement fix to robot
     send_msg = str(str(data)).encode()
     try:
